a0c3f62c0cd2e26b5b7ae3e87668c4b49912c6ef
Albert <alsorribes@gmail.com>
2025-12-22 22:39:56 +0100

    Create HealthCardID class - TEMPORARY file only for compiling the code

---
+++ src/data/HealthCardID.java
@@ -0,0 +1,35 @@
+package data;
+
+/**
+ * TEMPORARY -
+ * The personal identifying code in the National Health Service.
+ */
+public final class HealthCardID {
+    private final String personalID;
+
+    public HealthCardID(String code) {
+        this.personalID = code;
+    }
+
+    public String getPersonalID() {
+        return personalID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HealthCardID that = (HealthCardID) o;
+        return personalID.equals(that.personalID);
+    }
+
+    @Override
+    public int hashCode() {
+        return personalID.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "HealthCardID{" + "personalID='" + personalID + '\'' + '}';
+    }
+}
