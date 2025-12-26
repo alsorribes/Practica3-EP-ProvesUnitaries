8b099ff66f8a6d4bc24ac969373f94ce4269e438
miriam <miriamrc789@gmail.com>
2025-12-26 18:27:00 +0100

    Add MedicalHistoryTest with valid construction test

---
+++ src/test/domain/MedicalHistoryTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import data.HealthCardID;
+import exceptions.HealthCardIDException;
+import exceptions.IncorrectParametersException;
+import medicalconsultation.MedicalHistory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MedicalHistoryTest {
+
+    @Test
+    public void testValidMedicalHistoryConstruction() throws HealthCardIDException, IncorrectParametersException {
+        HealthCardID cip = new HealthCardID("1234567890ABCDEF");
+        MedicalHistory medicalHistory = new MedicalHistory(cip, 12345);
+
+        assertEquals(cip, medicalHistory.getCip());
+        assertEquals(12345, medicalHistory.getMembShipNumb());
+        assertEquals("", medicalHistory.getHistory());
+    }
+}
