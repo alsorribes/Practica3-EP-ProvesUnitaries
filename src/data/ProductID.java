b850d9b17fddd9adb439f82c6b14398160a5673d
MartiGB27 <mguilanyaborrellas@gmail.com>
2025-12-27 19:10:54 +0100

    Add exceptions in ProductID

--- src/data/ProductID.java
+++ src/data/ProductID.java
@@ -1,14 +1,31 @@
 package data;
 
+import exceptions.InvalidProductIDException;
+
 /**
  * Universal Product Code for medicines.
  */
 public final class ProductID {
     private final String code;
 
-    public ProductID(String code) {
+    public ProductID(String code) throws InvalidProductIDException {
+        // Validate that code is not null and not empty
+        if(code == null || code.trim().isEmpty()){
+            throw new InvalidProductIDException("ProductID cannot be null or empty.");
+        }
+
+        // Validate that code contains 16 alphanumeric characters
+        if(!isValidFormat(code)){
+            throw new InvalidProductIDException("ProductID has an invalid format: " + code);
+        }
+
         this.code = code;
     }
+
+    private boolean isValidFormat(String code){
+        return code.length() == 16 && code.matches("^[a-zA-Z0-9]{16}$");
+    }
+
 
     public String getCode() {
         return code;
