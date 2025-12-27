4fcb06ce6558926b81e4d4479a3b36723f649cc6
MartiGB27 <mguilanyaborrellas@gmail.com>
2025-12-27 16:41:14 +0100

    Create test ePrescripCodeTest

---
+++ src/test/data/ePrescripCodeTest.java
@@ -0,0 +1,29 @@
+package test.data;
+
+import data.ePrescripCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for ePrescripCode value object.
+ */
+@DisplayName("ePrescripCode - Unit Tests")
+public class ePrescripCodeTest {
+    // ----------- CONSTRUCTOR AND GETTER
+    @Test
+    @DisplayName("Constructor - Stores prescription code correctly")
+    void testConstructorStoresCode() {
+        // Arrange
+        String code = "EP-2025-000123";
+
+        // Act
+        ePrescripCode prescripCode = new ePrescripCode(code);
+
+        // Assert
+        assertEquals(code, prescripCode.getCode());
+    }
+
+}
